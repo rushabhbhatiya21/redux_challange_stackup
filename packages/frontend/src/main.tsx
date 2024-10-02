66356b0eb123216cae63cff17ca9981896b43210
rushabhbhatiya21 <rushabhbhatiya1997@gmail.com>
2024-10-02 11:37:42 +0530

    Login Register works, CreatePost does not work

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
-import App from "./App.tsx";
-import "./index.css";
+import App from "./App";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "./store.ts";
+import store from "./store";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<StrictMode>
 		<Provider store={store}>
-				<App />
+			<App />
 		</Provider>
 	</StrictMode>,
 );
